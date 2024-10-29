f9c9227229fc6f8cf8ca8403d93c16e008b274b6
Ibnushemsudin <musteshemsu@gmail.com>
2024-10-28 17:01:32 -0700

    Insertion teh main function

---
+++ c.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    DoublyLinkedList dll;
+
+    dll.insertAtBeginning(10);
+    dll.insertAtEnd(20);
+    dll.insertAtPosition(15, 1);
+
+    cout << "Forward traversal: ";
+    dll.traverseForward();  // Output: 10 15 20
+
+    cout << "Backward traversal: ";
+    dll.traverseBackward(); // Output: 20 15 10
+    dll.deleteFromBeginning();
+    dll.traverseForward();  // Output: 15 20
+
+    dll.deleteFromEnd();
+    dll.traverseForward();  // Output: 15
+
+    dll.insertAtEnd(25);
+    dll.insertAtEnd(30);
+    dll.deleteFromPosition(1);
+    dll.traverseForward();  // Output: 15 30
+
+    return 0;
+}
